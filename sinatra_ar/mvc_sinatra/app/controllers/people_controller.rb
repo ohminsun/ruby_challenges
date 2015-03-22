ad6b2b5281b6e58b9745eb01792d6abc7fff315d
ohminsun <ohminsun@gmail.com>
2015-03-22 17:36:57 +0000

    refactored person new and edit view forms with partial

--- sinatra_ar/mvc_sinatra/app/controllers/people_controller.rb
+++ sinatra_ar/mvc_sinatra/app/controllers/people_controller.rb
@@ -19,24 +19,29 @@ post '/people' do
   redirect "/people/#{person.id}"
 end
 
-get '/people/:id' do
-  @person = Person.find(params[:id])
-  birth_path_num = Person.get_birth_path_num(@person.birthdate.strftime("%m%d%Y"))
-  @message = Person.get_message(birth_path_num)  
-  erb :"/people/show"
-end
-
 get '/people/:id/edit' do
     @person = Person.find(params[:id])
     erb :"/people/edit"
 end
 
 put '/people/:id' do
-    person.birthdate = params[:birthdate]
-    person.save
-    redirect "/people/#{person.id}"    
-  end
+  person = Person.find(params[:id])
+  person.first_name = params[:first_name]
+  person.last_name = params[:last_name]
+  person.birthdate = params[:birthdate]
+  person.save
+  redirect "/people/#{person.id}"
+end
 
 delete '/people/:id' do
+    person = Person.find(params[:id])
+    person.delete 
     redirect "/people"
   end
+
+get '/people/:id' do
+  @person = Person.find(params[:id])
+  birth_path_num = Person.get_birth_path_num(@person.birthdate.strftime("%m%d%Y"))
+  @message = Person.get_message(birth_path_num)  
+  erb :"/people/show"
+end
