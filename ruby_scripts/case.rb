93fb2d39ad11fd24f8431eb9c8aba2c6b2f7e459
ohminsun <ohminsun@gmail.com>
2015-03-23 15:44:48 +0000

    created new folder to organize misc scripts

---
+++ ruby_scripts/case.rb
@@ -0,0 +1,20 @@
+weather = "snowy"
+
+case weather
+when "sunny"
+    puts "You should put on some sunglasses and go outside!"
+when "cloudy"
+    puts "Make sure you bring an umbrella in case it rains."
+when "rainy"
+    puts "Do you have an umbrella with you?"
+when "snowy"
+    puts "Bundle up, put on a hat and gloves, and make sure you wear your parka today."
+when "foggy"
+    puts "Remember to turn on the headlights on your car."
+when "hot"
+    puts "Drink a lot of water today so you stay hydrated."
+when "cold"
+    puts "Make some hot chocolate and stay inside."
+else
+    puts "I'm not sure what you should today with that type of weather, sorry."
+end
---
+++ ruby_scripts/weather.rb
@@ -0,0 +1,24 @@
+case ("raining")
+when "raining" then puts "Wear rubber boots!"
+when "snowing" then puts "Best to stay home where it's warm."
+when "sunny" then puts "Hooray!  Slap on some sunscreen!"
+else puts "Who knows?  Best to prepare for ANYTHING."
+end
+   
+fav_color = "red"
+case fav_color
+when "red"
+    puts "Red like fire!"
+when "orange"
+    puts "Orange like an ...orange."
+when "yellow"
+    puts "Yellow daffodils are so pretty in the spring!"
+when "green"
+    puts "Have you been to the Emerald City in OZ?"
+when "blue"
+    puts "Blue like the sky!"
+when "purple"
+    puts "Purple plums are totally the tastiest."
+else 
+    puts "I don't know what color that is, sorry!"
+end
