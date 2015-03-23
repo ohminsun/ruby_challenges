ce66e39a0cc0138ee74b844167a2e1b401347d27
ohminsun <ohminsun@gmail.com>
2015-03-23 15:32:29 +0000

    updated people edit form and added error msg

--- sinatra_ar/mvc_sinatra/app/helpers/application_helper.rb
+++ sinatra_ar/mvc_sinatra/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    def people_form_method(person)
+        if person.new_record?
+            @method = "post"
+        else
+            @method = "put"
+        end
+        @method
+    end
 end
+
+
+
