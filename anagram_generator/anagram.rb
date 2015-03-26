809355860846cf50c12a890565f76f3fdc5c1636
ohminsun <ohminsun@gmail.com>
2015-03-26 19:11:37 +0000

    corrected anagram enumerables

--- anagram_generator/anagram.rb
+++ anagram_generator/anagram.rb
@@ -1,26 +1,19 @@
-def reverse_letters(letters)
-    length = letters.length
-    reversed_letters = Array.new(length)
-    
-    letters.each_with_index do |letter, index|
-        reversed_letters[length - index - 1] = letter
-    end
-    
-    reverse_letters
-end
-
 require_relative 'reverse_letters'
+ 
+def find_anagram(string)
+  letters = string.split(//)
+ 
+  anagrams = []
+ 
+  letters.each do |letter|
+    remaining = letters.select { |l| l != letter }
+ 
+    anagrams << letter + remaining.join('')
+ 
+    anagrams << letter + reverse_letters(remaining).join('')
+  end
 
-def find_anagram(letters)
-    length = letters.length
-    anagram = Array.new(length)
-    
-    letters.chunk {index}
-end
-
-find_anagram
-end
-    
-    
+  anagrams
+end    
  
 
