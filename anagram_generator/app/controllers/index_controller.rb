176111cbbe0489f254c7811fa0ac20f8eb102a76
ohminsun <ohminsun@gmail.com>
2015-03-28 15:49:59 +0000

    altered index controller to bind sinatra

--- anagram_generator/app/controllers/index_controller.rb
+++ anagram_generator/app/controllers/index_controller.rb
@@ -1,3 +1,17 @@
+require "sinatra"
+set :bind, '0.0.0.0'
+
 get '/'
     erb: index
+end
+
+get '/anagrams/:word' do
+  @word = params[:word]
+  @anagrams = Word.find_anagrams(@word)
+  erb :show
+end
+
+post '/' do
+  @word = params[:word]
+  redirect "/anagrams/#{@word}"
 end
---
+++ anagram_generator/app/models/word.rb
@@ -0,0 +1,26 @@
+class Word
+  
+  require_relative 'reverse_letters'  
+     
+  def self.find_anagram(string)
+  letters = string.split(//)
+ 
+  anagrams = []
+ 
+  letters.each do |letter|
+    remaining = letters.select { |l| l != letter }
+ 
+    anagrams << letter + remaining.join('')
+ 
+    anagrams << letter + reverse_letters(remaining).join('')
+  end
+     
+  def self.reverse_letters(letters)
+    length = letters.length
+    reversed_letters = Array.new(length)
+    
+    letters.each_with_index do |letter, index|
+        reversed_letters[length - index - 1] = letter
+    end
+     
+end
